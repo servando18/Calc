6e356b51a23c04396a66dda0216a57d3490da100
servando18 <100006660+servando18@users.noreply.github.com>
2023-11-01 22:27:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
